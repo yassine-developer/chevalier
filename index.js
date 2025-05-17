b02168c065194d078b79da8c28a4327e2816c600
yassine-developer <mah.yassine1981@gmail.com>
2025-05-17 16:49:24 +0200

    les fonctionnalites de base

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Chevalier {
-    constructor(name, strength, magic, life=100, mana=50, potions=2){
+    constructor(name, strength, magic, life = 100, mana = 50, potions = 2) {
         this.name = name;
         this.strength = strength;
         this.magic = magic;
@@ -8,23 +8,23 @@ class Chevalier {
         this.potions = potions;
     }
 
-    shout(){
+    shout() {
         return `${this.name} est pret pour le combat !`;
     }
 
-    attack(target){
+    attack(target) {
         let degats;
-        if(target.strength - this.strength > 0){
-            degats = (target.strength - this.strength) / 2;
+        if (target.strength - this.strength > 0) {
+            degats = Math.round((target.strength - this.strength) / 2);
         } else {
-            degats = (this.strength - target.strength) ;
+            degats = (this.strength - target.strength);
         }
         target.getDamages(degats);
         return `${this.name} attaque ${target.name} et lui inflige ${degats} points de dégâts.`;
     }
 
-    magicAttack(target){
-        if(this.mana < 20){
+    magicAttack(target) {
+        if (this.mana < 20) {
             return `${this.name} n'a pas assez de mana pour lancer un sort.`;
         }
         this.mana -= 20;
@@ -32,29 +32,30 @@ class Chevalier {
         return `${this.name} lance un sort sur ${target.name} et lui inflige ${this.magic} points de dégâts.`;
     }
 
-    getDamages(amount){
+    getDamages(amount) {
         this.life -= amount;
-        if(this.life < 0) {
+        if (this.life < 0) {
             this.life = 0;
-        }else{
-        return `${this.name} subit ${amount} points de dégâts, PV restants : ${this.life.toFixed(2)}`;
         }
+        // else{
+        // return `${this.name} subit ${amount} points de dégâts, PV restants : ${this.life.toFixed(2)}`;
+        // }
     }
 
-    usePotion(){
-        if(this.potions <= 0){
+    usePotion() {
+        if (this.potions <= 0) {
             return `${this.name} n'a plus de potions.`;
         }
-        if(this.life === 100){
+        if (this.life === 100) {
             return `${this.name} a déjà tous ses PV.`;
         }
         this.potions--;
         this.life += 30;
-        if(this.life > 100) this.life = 100;
-        return `${this.name} utilise une potion, PV actuels : ${this.life.toFixed(2)}, potions restantes : ${this.potions}`;
+        if (this.life > 100) this.life = 100;
+        return `${this.name} utilise une potion, PV actuels : ${this.life}, potions restantes : ${this.potions}`;
     }
 
-    isDead(){
+    isDead() {
         return this.life <= 0;
     }
 }
@@ -63,21 +64,21 @@ class Chevalier {
 document.getElementById('knights').style.display = 'block';
 document.querySelector('.container').style.display = 'none';
 
-let chevalier1  , chevalier2;
+let chevalier1, chevalier2;
 
-document.getElementById('knights').addEventListener('submit', function(e) {
-    e.preventDefault(); 
+document.getElementById('knights').addEventListener('submit', function (e) {
+    e.preventDefault();
 
     // Recuperer les donnees pour les chevaliers
     const name1 = document.getElementById('nameInput1').value;
     const strength1 = parseInt(document.getElementById('strength1').value);
-    const magic1 = parseInt(document.getElementById('magic1').value);
+    const magic1 = parseInt(document.getElementById('magicInput1').value);
 
     const name2 = document.getElementById('nameInput2').value;
     const strength2 = parseInt(document.getElementById('strength2').value);
-    const magic2 = parseInt(document.getElementById('magic2').value);
+    const magic2 = parseInt(document.getElementById('magicInput2').value);
 
-    
+
     chevalier1 = new Chevalier(name1, strength1, magic1);
     chevalier2 = new Chevalier(name2, strength2, magic2);
 
@@ -87,10 +88,11 @@ document.getElementById('knights').addEventListener('submit', function(e) {
 
     // Mettre a jour l'interface
     updateInterface();
-    console.log(chevalier1.name);
+    logFight(chevalier1.shout());
+    logFight(chevalier2.shout());
 });
 
-function updateInterface(){
+function updateInterface() {
 
     //mettre a jour l'interface pour le chavalier1
     document.getElementById('name1').textContent = chevalier1.name;
@@ -116,10 +118,58 @@ function updateInterface(){
 }
 
 //les log du combat
-function logFight(message){
+function logFight(message) {
     const logElement = document.querySelector("#log");
-        //li faut afficher les messages recu lorsque les bouttons sont actionner
-        logElement.innerHTML = message;
-    
+    //li faut afficher les messages recu lorsque les bouttons sont actionner
+    logElement.innerHTML += message + "<br>";
 }
+
+
+
+
+//les actions des bouttons
+
+document.querySelector("#attack1").addEventListener('click', () => {
+    logFight(chevalier1.attack(chevalier2));
+    updateInterface();
+    if (chevalier2.isDead()) {
+        logFight(`${chevalier2.name} est mort. ${chevalier1.name} gagne le combat!`)
+    }
+})
+
+document.querySelector("#magic1").addEventListener('click', () => {
+    logFight(chevalier1.magicAttack(chevalier2));
+    updateInterface();
+    if (chevalier2.isDead()) {
+        logFight(`${chevalier2.name} est mort. ${chevalier1.name} gagne le combat!`)
+    }
+})
+
+document.querySelector("#potion1").addEventListener(('click'), () => {
+
+    logFight(chevalier1.usePotion());
+    updateInterface();
+})
+
+document.querySelector("#attack2").addEventListener('click', () => {
+    logFight(chevalier2.attack(chevalier1));
+    updateInterface();
+    if (chevalier1.isDead()) {
+        logFight(`${chevalier1.name} est mort. ${chevalier2.name} gagne le combat!`)
+    }
+})
+
+document.querySelector("#magic2").addEventListener('click', () => {
+    logFight(chevalier2.magicAttack(chevalier1));
+    updateInterface();
+    if (chevalier1.isDead()) {
+        logFight(`${chevalier1.name} est mort. ${chevalier2.name} gagne le combat!`)
+    }
+})
+
+document.querySelector("#potion2").addEventListener(('click'), () => {
+
+    logFight(chevalier2.usePotion());
+    updateInterface();
+})
 
